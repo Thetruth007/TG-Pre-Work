7922ef7a6b86a38ce9a784f8501082b553d613d8
Romero <>
2018-08-15 17:58:20 -0600

    Instance

--- index.html/codecademy.js
+++ index.html/codecademy.js
@@ -110,3 +110,13 @@ class Surgeon {
     this.department = department;
   }
 }
+
+class Surgeon {
+  constructor(name, department) {
+    this.name = name;
+    this.department = department;
+  }
+}
+
+const surgeonCurry = new Surgeon('Curry', 'Cardiovascular');
+const surgeonDurant = new Surgeon('Durant', 'Orthopedics');
