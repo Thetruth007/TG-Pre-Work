0a77f26642308c3546346c3c08e324c8a3c844f9
Romero <>
2018-08-22 22:18:32 -0600

    Create a Variable II Full code

--- index.html/codecademy.js
+++ index.html/codecademy.js
@@ -159,3 +159,11 @@ var age = 31;
 console.log(age);
 
 var hasPet = true;
+
+var strength = '50,000 pounds';
+var age = 31;
+var hasPet = true;
+
+console.log(hasPet);
+console.log(age);
+console.log('How much stuff can a variable hold? ', strength);
