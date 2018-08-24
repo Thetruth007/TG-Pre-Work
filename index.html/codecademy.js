36a7e3937a043fa7499c18f0a6144a9ebdd2c7dd
Romero <>
2018-08-24 12:09:36 -0600

    Babel Source Lib

--- index.html/codecademy.js
+++ index.html/codecademy.js
@@ -655,3 +655,20 @@ let esSixTemplateLiterals = 87.61;
 
 var carbonara = `You can make carbonara with ${pasta}, ${meat}, and a sauce made with ${sauce}.`;
 var carbonara = pasta + meat + sauce;
+
+/*{
+  "name": "learning-babel",
+  "version": "1.0.0",
+  "description": "Use Babel to transpile JavaScript ES6 to ES5",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1",
+    "build": "babel src -d lib"
+  },
+  "author": "",
+  "license": "ISC",
+  "devDependencies": {
+    "babel-cli": "^6.26.0",
+    "babel-preset-env": "^1.7.0"
+  }
+} */
