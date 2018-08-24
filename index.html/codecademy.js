dcb5f5bd4b2573ca251256b4db1a687f610ad6d9
Romero <>
2018-08-24 11:51:53 -0600

    Inheritance V

--- index.html/codecademy.js
+++ index.html/codecademy.js
@@ -621,3 +621,18 @@ const nurseOlynyk = new Nurse('Olynyk', ['Trauma', 'Pediatrics']);
 
 nurseOlynyk.takeVacationDays(5);
 console.log(nurseOlynyk.remainingVacationDays);
+
+class Nurse extends HospitalEmployee {
+  constructor(name, certifications) {
+    super(name);
+    this._certifications = certifications;
+  }
+
+  get certifications() {
+    return this._certifications;
+  }
+
+  addCertification(newCertification) {
+    this._certifications.push(newCertification);
+  }
+}
