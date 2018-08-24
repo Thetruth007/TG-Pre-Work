9b8cd65e294a93c93e9e641b7d7fd9a32706ad5d
Romero <>
2018-08-24 11:50:49 -0600

    Inheritance IV

--- index.html/codecademy.js
+++ index.html/codecademy.js
@@ -618,3 +618,6 @@ class Nurse extends HospitalEmployee {
 }
 
 const nurseOlynyk = new Nurse('Olynyk', ['Trauma', 'Pediatrics']);
+
+nurseOlynyk.takeVacationDays(5);
+console.log(nurseOlynyk.remainingVacationDays);
