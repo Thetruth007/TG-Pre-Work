41d15c3cd06e7b369b35a5103bf06177278785ed
Romero <>
2018-08-24 11:49:51 -0600

    Inheritance III

--- index.html/codecademy.js
+++ index.html/codecademy.js
@@ -590,3 +590,31 @@ const surgeonDurant = new Surgeon('Durant', 'Orthopedics');
 console.log(surgeonCurry.name);
 surgeonCurry.takeVacationDays(3);
 console.log(surgeonCurry.remainingVacationDays);
+
+class HospitalEmployee {
+  constructor(name) {
+    this._name = name;
+    this._remainingVacationDays = 20;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get remainingVacationDays() {
+    return this._remainingVacationDays;
+  }
+
+  takeVacationDays(daysOff) {
+    this._remainingVacationDays -= daysOff;
+  }
+}
+
+class Nurse extends HospitalEmployee {
+  constructor(name, certifications) {
+    super(name);
+    this._certifications = certifications;
+  }
+}
+
+const nurseOlynyk = new Nurse('Olynyk', ['Trauma', 'Pediatrics']);
