11ed2f59bf3599f5261951355f3a971d73525859
Romero <>
2018-08-24 09:03:19 -0600

    console.log final code

--- index.html/codecademy.js
+++ index.html/codecademy.js
@@ -367,3 +367,13 @@ groceryList.shift();
 groceryList.unshift('popcorn');
 
 console.log(groceryList.slice(1, 4));
+
+let groceryList = ['orange juice', 'bananas', 'coffee beans', 'brown rice', 'pasta', 'coconut oil', 'plantains'];
+
+groceryList.shift();
+
+groceryList.unshift('popcorn');
+console.log(groceryList);
+
+console.log(groceryList.slice(1, 4));
+console.log(groceryList);
