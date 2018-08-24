c9d280ba5b06a7ee2328bc75599d45c84182cadc
Romero <>
2018-08-23 22:54:15 -0600

    Parameters II take order

--- index.html/codecademy.js
+++ index.html/codecademy.js
@@ -227,3 +227,11 @@ takeOrder("mushrooms");
 const takeOrder = (topping, crustType) => {
   console.log('Order: ' + crustType + ' pizza topped with ' + topping);
 };
+
+const takeOrder = (topping, crustType) => {
+  console.log('Order: ' + crustType + ' pizza topped with ' + topping);
+};
+
+takeOrder('mushroom', 'thin crust');
+takeOrder('spinach', 'whole wheat');
+takeOrder('pepperoni', 'brooklyn style');
