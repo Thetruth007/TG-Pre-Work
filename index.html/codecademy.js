194fbbea1b33b480e36dfba00959631c5057124d
Romero <>
2018-08-24 11:04:22 -0600

    for / if console.log nested loop

--- index.html/codecademy.js
+++ index.html/codecademy.js
@@ -405,3 +405,11 @@ for (let vacationSpotIndex = vacationSpots.length - 1; vacationSpotIndex >= 0; v
 let myPlaces = ['Houston', 'Sioux Falls', 'Phoenix'];
 let friendPlaces = ['Sioux Falls', 'Missoula', 'Buffalo'];
 for (let myPlacesIndex = 0; myPlacesIndex < myPlaces.length; myPlacesIndex++)
+
+{
+  for (let friendPlacesIndex = 0; friendPlacesIndex < friendPlaces.length; friendPlacesIndex++) {
+		if (myPlaces[myPlacesIndex] === friendPlaces[friendPlacesIndex]) {
+      console.log(friendPlaces[friendPlacesIndex]);
+    }
+  }
+}
