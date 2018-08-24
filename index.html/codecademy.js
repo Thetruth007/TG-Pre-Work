6dbd33695f0b4cd5585e5e07cc6de4330cf0400f
Romero <>
2018-08-24 11:06:14 -0600

    While loops

--- index.html/codecademy.js
+++ index.html/codecademy.js
@@ -413,3 +413,12 @@ for (let myPlacesIndex = 0; myPlacesIndex < myPlaces.length; myPlacesIndex++)
     }
   }
 }
+
+let cards = ['Diamond', 'Spade', 'Heart', 'Club'];
+
+let currentCard = 'Heart';
+while (currentCard !== 'Spade') {
+  console.log(currentCard);
+  currentCard = cards[Math.floor(Math.random() * 4)];
+}
+console.log("found a spade")
